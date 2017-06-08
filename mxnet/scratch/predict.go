e8697a6c27637b753b7fdd71253fc3c118dfb9e9
Cheng Li <chengli@Chengs-MacBook-Pro.local>
2017-06-07 20:37:17 -0400

    fix shapes

--- mxnet/scratch/predict.go
+++ mxnet/scratch/predict.go
@@ -1,17 +1,3 @@
-// Copyright 2016 go-mxnet-predictor Authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
 import (
@@ -39,8 +25,8 @@ const path = "/Users/chengli/Downloads"
 var (
 	caffenet    = []uint32{1, 3, 224, 224}
 	rn1015k500  = []uint32{1, 3, 224, 224}
-	vgg19       = []uint32{1, 1, 224, 224}
-	inceptionbn = []uint32{1, 1, 299, 299}
+	vgg19       = []uint32{1, 3, 224, 224}
+	inceptionbn = []uint32{1, 3, 224, 224}
 )
 
 func distance(p1 []float64, p2 []float64) float64 {
