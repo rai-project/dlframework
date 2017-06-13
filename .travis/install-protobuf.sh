3fd480f453b2e3d4b002b5f881387f2634bdc20b
Abdul Dakkak <abduld@wolfram.com>
2017-06-13 18:59:25 +0100

    ..

--- .travis/install-protobuf.sh
+++ .travis/install-protobuf.sh
@@ -2,4 +2,4 @@
 set -ex
 # check to see if protobuf folder is empty
 wget https://github.com/google/protobuf/releases/download/v$PROTOBUF_VER/protoc-$PROTOBUF_VER-linux-x86_64.zip
-unzip protoc-$PROTOBUF_VER-linux-x86_64.zip -o -d $HOME/protobuf/
+unzip protoc-$PROTOBUF_VER-linux-x86_64.zip -d $HOME/protobuf/
