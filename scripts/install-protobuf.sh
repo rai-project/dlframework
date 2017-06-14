e7f293827b13286c730f7ee43be0498411fa584e
Abdul Dakkak <abduld@wolfram.com>
2017-06-14 15:38:01 +0100

    use cache

--- scripts/install-protobuf.sh
+++ scripts/install-protobuf.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 set -ex
 # check to see if protobuf folder is empty
-wget https://github.com/google/protobuf/releases/download/v$PROTOBUF_VER/protoc-$PROTOBUF_VER-linux-x86_64.zip
-unzip protoc-$PROTOBUF_VER-linux-x86_64.zip -d $HOME/protobuf/
+if [ ! -d "$HOME/protobuf/lib" ]; then
+  wget https://github.com/google/protobuf/releases/download/v$PROTOBUF_VER/protoc-$PROTOBUF_VER-linux-x86_64.zip
+  unzip protoc-$PROTOBUF_VER-linux-x86_64.zip -d $HOME/protobuf/
+else
+  echo "Using cached directory."
+fi
