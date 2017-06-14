83cc45b6fafb8f8a660ad05807871f5bbc3fb9a9
Abdul Dakkak <abduld@wolfram.com>
2017-06-15 00:49:51 +0100

    updates

--- frameworks/common/registry.go
+++ frameworks/common/registry.go
@@ -1,41 +1,39 @@
 package common
 
-
 import (
+	"github.com/rai-project/dlframework"
 	context "golang.org/x/net/context"
-	grpc "google.golang.org/grpc"
 )
 
 type Registry struct {
-
 }
 
 func (c *Registry) GetFrameworkManifests(ctx context.Context, ignore *dlframework.Null) (*dlframework.GetFrameworkManifestsResponse, error) {
-  panic("GetFrameworkManifests")
-  return nil , nil
+	panic("GetFrameworkManifests")
+	return nil, nil
 }
 
-func (c *Registry) 	GetFrameworkManifest(ctx context.Context, req *dlframework.GetFrameworkManifestRequest) (*dlframework.FrameworkManifest, error){
-  panic("GetFrameworkManifest")
-  return nil , nil
+func (c *Registry) GetFrameworkManifest(ctx context.Context, req *dlframework.GetFrameworkManifestRequest) (*dlframework.FrameworkManifest, error) {
+	panic("GetFrameworkManifest")
+	return nil, nil
 }
 
-func (c *Registry) 	GetFrameworkModels(ctx context.Context, req *dlframework.GetFrameworkManifestRequest) (*dlframework.GetModelManifestsResponse, error){
-  panic("GetFrameworkModels")
-  return nil , nil
+func (c *Registry) GetFrameworkModels(ctx context.Context, req *dlframework.GetFrameworkManifestRequest) (*dlframework.GetModelManifestsResponse, error) {
+	panic("GetFrameworkModels")
+	return nil, nil
 }
 
-func (c *Registry) 	GetModelManifests(ctx context.Context, ignore *Null) (*dlframework.GetModelManifestsResponse, error){
-  panic("GetModelManifests")
-  return nil , nil
+func (c *Registry) GetModelManifests(ctx context.Context, ignore *dlframework.Null) (*dlframework.GetModelManifestsResponse, error) {
+	panic("GetModelManifests")
+	return nil, nil
 }
 
-func (c *Registry) 	GetFrameworkModelManifest(ctx context.Context, *dlframework.GetFrameworkModelManifestRequest) (*dlframework.ModelManifest, error){
-  panic("GetFrameworkModelManifest")
-  return nil , nil
+func (c *Registry) GetFrameworkModelManifest(ctx context.Context, req *dlframework.GetFrameworkModelManifestRequest) (*dlframework.ModelManifest, error) {
+	panic("GetFrameworkModelManifest")
+	return nil, nil
 }
 
-func (c *Registry) 	GetModelManifest(ctx context.Context, *dlframework.GetModelManifestRequest) (*dlframework.ModelManifest, error){
-  panic("GetModelManifest")
-  return nil , nil
+func (c *Registry) GetModelManifest(ctx context.Context, req *dlframework.GetModelManifestRequest) (*dlframework.ModelManifest, error) {
+	panic("GetModelManifest")
+	return nil, nil
 }
