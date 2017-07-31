b84edaee935471181a0b2a02600e372c4c9c6678
Abdul Dakkak <abduld@wolfram.com>
2017-07-31 09:53:43 -0400

    work

--- framework/agent/agent.go
+++ framework/agent/agent.go
@@ -4,17 +4,13 @@ import (
 	"path"
 	"strings"
 	"sync"
-	"time"
 
 	"github.com/k0kubun/pp"
 	"github.com/rai-project/config"
 	dl "github.com/rai-project/dlframework"
 	store "github.com/rai-project/libkv/store"
+	lock "github.com/rai-project/lock/registry"
 	"github.com/rai-project/registry"
-)
-
-var (
-	DefaultTTL = time.Hour
 )
 
 type Base struct {
@@ -26,6 +22,15 @@ func toPath(s string) string {
 }
 
 func (b *Base) PublishInRegistery(prefix string) error {
+	var wg sync.WaitGroup
+
+	framework := b.Framework
+	cn, err := framework.CanonicalName()
+	if err != nil {
+		return err
+	}
+
+	ttl := registry.Config.Timeout
 	marshaler := registry.Config.Serializer
 
 	rgs, err := registry.New()
@@ -34,73 +39,53 @@ func (b *Base) PublishInRegistery(prefix string) error {
 	}
 	defer rgs.Close()
 
-	framework := b.Framework
-	cn, err := framework.CanonicalName()
+	prefix = path.Join(config.App.Name, prefix)
+
+	rgs.Put(prefix, nil, &store.WriteOptions{IsDir: true})
+
+	locker := lock.New(rgs)
+	locker.Lock(prefix)
+	defer locker.Unlock(prefix)
+
+	frameworksKey := path.Join(prefix, "frameworks")
+
+	kv, err := rgs.Get(frameworksKey)
+	if err != nil {
+		if ok, e := rgs.Exists(frameworksKey); e == nil && ok {
+			log.WithError(err).Errorf("cannot get value for key %v", frameworksKey)
+			return err
+		}
+		kv = &store.KVPair{
+			Key:   frameworksKey,
+			Value: []byte{},
+		}
+	}
+	found := false
+	val := strings.TrimSpace(string(kv.Value))
+	frameworkLines := strings.Split(val, "\n")
+	for _, name := range frameworkLines {
+		if name == cn {
+			found = true
+			break
+		}
+	}
+	if !found {
+		frameworkLines = append(frameworkLines, cn)
+		newVal := strings.TrimSpace(strings.Join(frameworkLines, "\n"))
+		rgs.AtomicPut(frameworksKey, []byte(newVal), kv, nil)
+	}
+
+	key := path.Join(prefix, toPath(cn))
+	rgs.Put(key, nil, &store.WriteOptions{TTL: ttl, IsDir: true})
+
+	key = path.Join(key, "manifest.json")
+	bts, err := marshaler.Marshal(&framework)
 	if err != nil {
 		return err
 	}
-
-	var wg sync.WaitGroup
-	prefix = path.Join(config.App.Name, prefix)
-	rgs.Put(prefix, nil, &store.WriteOptions{IsDir: true})
-	{
-		frameworksKey := path.Join(prefix, "frameworks")
-		// lock, err := rgs.NewLock(frameworksKey, &store.LockOptions{TTL: 2 * time.Second})
-		// if err != nil {
-		// 	return errors.Wrapf(err, "cannot get lock for key %v", frameworksKey)
-		// }
-		// pp.Println("trying lock....")
-		// _, err = lock.Lock(nil)
-		// if err != nil {
-		// 	return errors.Wrapf(err, "cannot lock key %v", frameworksKey)
-		// }
-		// pp.Println("locking....")
-		wg.Add(1)
-		go func() {
-			// defer lock.Unlock()
-			defer wg.Done()
-			kv, err := rgs.Get(frameworksKey)
-			if err != nil {
-				if ok, e := rgs.Exists(frameworksKey); e == nil && ok {
-					log.WithError(err).Errorf("cannot get value for key %v", frameworksKey)
-					return
-				}
-				kv = &store.KVPair{
-					Key:   frameworksKey,
-					Value: []byte{},
-				}
-			}
-			found := false
-			val := strings.TrimSpace(string(kv.Value))
-			frameworkLines := strings.Split(val, "\n")
-			for _, name := range frameworkLines {
-				if name == cn {
-					found = true
-					break
-				}
-			}
-			if !found {
-				frameworkLines = append(frameworkLines, cn)
-				newVal := strings.TrimSpace(strings.Join(frameworkLines, "\n"))
-				rgs.AtomicPut(frameworksKey, []byte(newVal), kv, nil)
-			}
-		}()
+	if err := rgs.Put(key, bts, &store.WriteOptions{TTL: ttl, IsDir: false}); err != nil {
+		return err
 	}
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		key := path.Join(prefix, toPath(cn))
-		rgs.Put(key, nil, &store.WriteOptions{TTL: DefaultTTL, IsDir: true})
-
-		key = path.Join(key, "manifest.json")
-		bts, err := marshaler.Marshal(&framework)
-		if err != nil {
-			return
-		}
-		if err := rgs.Put(key, bts, &store.WriteOptions{TTL: DefaultTTL, IsDir: false}); err != nil {
-			return
-		}
-	}()
 
 	models := framework.Models()
 	wg.Add(len(models))
@@ -117,7 +102,7 @@ func (b *Base) PublishInRegistery(prefix string) error {
 				return
 			}
 			key := path.Join(prefix, toPath(mn), "manifest.json")
-			rgs.Put(key, bts, &store.WriteOptions{TTL: DefaultTTL, IsDir: false})
+			rgs.Put(key, bts, &store.WriteOptions{TTL: ttl, IsDir: false})
 		}(model)
 	}
 
