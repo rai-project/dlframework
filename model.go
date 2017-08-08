6d1529556198db2d197b322546adbf8e3472a9c9
abdul dakkak <abduld@wolfram.com>
2017-08-08 16:48:54 -0500

    Update model.go

--- model.go
+++ model.go
@@ -195,7 +195,7 @@ func (m *ModelManifest) WorkDir() (string, error) {
 	}
 	cannonicalName = strings.Replace(cannonicalName, ":", "_", -1)
 	cannonicalName = strings.Replace(cannonicalName, " ", "_", -1)
-	workDir := filepath.Join(config.App.TempDir, cannonicalName)
+	workDir := filepath.Join(config.App.TempDir, "dlframework", cannonicalName)
 	if !com.IsDir(workDir) {
 		if err := os.MkdirAll(workDir, 0700); err != nil {
 			return "", errors.Wrapf(err, "failed to create model work directory %v", workDir)
