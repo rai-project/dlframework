463cb0e721a9e5e428976494095a1c1bf22297ec
Abdul Dakkak <abduld@wolfram.com>
2017-09-05 10:09:03 -0500

    fix type

--- prediction.go
+++ prediction.go
@@ -5,33 +5,33 @@ import (
 	"sort"
 )
 
-type PredictionFeatures []*PredictionFeature
+type Features []*Feature
 
 // Len is the number of elements in the collection.
-func (p PredictionFeatures) Len() int {
+func (p Features) Len() int {
 	return len(p)
 }
 
 // Less reports whether the element with
 // index i should sort before the element with index j.
-func (p PredictionFeatures) Less(i, j int) bool {
+func (p Features) Less(i, j int) bool {
 	pi := p[i].Probability
 	pj := p[j].Probability
 	return !(pi < pj || math.IsNaN(float64(pi)) && !math.IsNaN(float64(pj)))
 }
 
 // Swap swaps the elements with indexes i and j.
-func (p PredictionFeatures) Swap(i, j int) {
+func (p Features) Swap(i, j int) {
 	p[i], p[j] = p[j], p[i]
 }
 
-func (p PredictionFeatures) Sort() {
+func (p Features) Sort() {
 	sort.Sort(p)
 }
 
-func (p PredictionFeatures) Take(n int) PredictionFeatures {
+func (p Features) Take(n int) Features {
 	if p.Len() <= n {
 		return p
 	}
-	return PredictionFeatures(p[:n])
+	return Features(p[:n])
 }
