f33162976f4cf50beb4e1dbf372889c50e3f63e8
Abdul Dakkak <abduld@wolfram.com>
2017-09-06 11:34:54 -0500

    add for each

--- steps/foreach.go
+++ steps/foreach.go
@@ -1,1 +1,43 @@
 package steps
+
+import (
+	"golang.org/x/net/context"
+
+	"github.com/pkg/errors"
+	"github.com/rai-project/pipeline"
+)
+
+type forEach struct {
+	base
+	f func(in interface{}) interface{}
+}
+
+func NewForEach(ctx context.Context, f func(in interface{}) interface{}) (pipeline.Step, error) {
+	return forEach{
+		f: f,
+	}, nil
+}
+
+func (p forEach) New(ctx context.Context) (pipeline.Step, error) {
+	return p, nil
+}
+
+func (p forEach) Info() string {
+	return "ForEach"
+}
+
+func (p forEach) do(ctx context.Context, in0 interface{}) interface{} {
+	in, err := toSlice(in0)
+	if err != nil {
+		return errors.Errorf("expecting a slice input for CastToFloat32Slice, but got %v", in0)
+	}
+	res := make([]interface{}, len(in))
+	for ii, e := range in {
+		res[ii] = p.f(e)
+	}
+	return res
+}
+
+func (p forEach) Close() error {
+	return nil
+}
