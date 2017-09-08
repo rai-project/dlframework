3f90be007417c6f487321bb8dcc9d76a56f0a026
Cheng Li <cli99@illinois.edu>
2017-09-08 17:36:18 -0500

    add dummy predictor

--- steps/image_predict.go
+++ steps/image_predict.go
@@ -1,1 +1,33 @@
 package steps
+
+import (
+	"golang.org/x/net/context"
+
+	"github.com/rai-project/dlframework/framework/predict"
+	"github.com/rai-project/pipeline"
+)
+
+type predictImage struct {
+	base
+	predictor predict.Predictor
+}
+
+func NewPredictImage(predictor predict.Predictor) pipeline.Step {
+	res := predictImage{}
+	res.predictor = predictor
+	res.doer = res.do
+
+	return res
+}
+
+func (p predictImage) Info() string {
+	return "predictImage"
+}
+
+func (p predictImage) do(ctx context.Context, in0 interface{}) interface{} {
+	return nil
+}
+
+func (p predictImage) Close() error {
+	return nil
+}
