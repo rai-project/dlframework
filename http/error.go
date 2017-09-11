ab2a466e09e629ea0c114c63e78072629418c851
Abdul Dakkak <abduld@wolfram.com>
2017-09-10 19:49:28 -0500

    simplify

--- http/error.go
+++ http/error.go
@@ -6,9 +6,10 @@ import (
 	"net/http"
 	"strings"
 
-	runtimeerrors "github.com/go-openapi/errors"
 	"github.com/go-openapi/runtime"
 	"github.com/pkg/errors"
+
+	rerrors "github.com/go-openapi/errors"
 )
 
 type Error struct {
@@ -78,64 +79,7 @@ func ServeError(rw http.ResponseWriter, r *http.Request, err error) {
 	switch e := err.(type) {
 	case *Error:
 		e.WriteResponse(rw, runtime.JSONProducer())
-	case *runtimeerrors.CompositeError:
-		er := flattenComposite(e)
-		ServeError(rw, r, er.Errors[0])
-	case *runtimeerrors.MethodNotAllowedError:
-		rw.Header().Add("Allow", strings.Join(err.(*runtimeerrors.MethodNotAllowedError).Allowed, ","))
-		rw.WriteHeader(asHTTPCode(int(e.Code())))
-		if r == nil || r.Method != "HEAD" {
-			rw.Write(errorAsJSON(e))
-		}
-	case runtimeerrors.Error:
-		if e == nil {
-			rw.WriteHeader(http.StatusInternalServerError)
-			rw.Write(errorAsJSON(runtimeerrors.New(http.StatusInternalServerError, "Unknown error")))
-			return
-		}
-		rw.WriteHeader(asHTTPCode(int(e.Code())))
-		if r == nil || r.Method != "HEAD" {
-			rw.Write(errorAsJSON(e))
-		}
 	default:
-		rw.WriteHeader(http.StatusInternalServerError)
-		if r == nil || r.Method != "HEAD" {
-			rw.Write(errorAsJSON(runtimeerrors.New(http.StatusInternalServerError, err.Error())))
-		}
+		rerrors.ServeError(rw, r, err)
 	}
-}
-
-func flattenComposite(errs *runtimeerrors.CompositeError) *runtimeerrors.CompositeError {
-	var res []error
-	for _, er := range errs.Errors {
-		switch e := er.(type) {
-		case *runtimeerrors.CompositeError:
-			if len(e.Errors) > 0 {
-				flat := flattenComposite(e)
-				if len(flat.Errors) > 0 {
-					res = append(res, flat.Errors...)
-				}
-			}
-		default:
-			if e != nil {
-				res = append(res, e)
-			}
-		}
-	}
-	return runtimeerrors.CompositeValidationError(res...)
-}
-
-func errorAsJSON(err runtimeerrors.Error) []byte {
-	b, _ := json.Marshal(struct {
-		Code    int32  `json:"code"`
-		Message string `json:"message"`
-	}{err.Code(), err.Error()})
-	return b
-}
-
-func asHTTPCode(input int) int {
-	if input >= 600 {
-		return 422
-	}
-	return input
 }
