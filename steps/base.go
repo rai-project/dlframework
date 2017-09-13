e1ba4f0c5d99186b7989c77055f225bed9f6ed77
Abdul Dakkak <abduld@wolfram.com>
2017-09-13 11:21:22 -0500

    recover from panics in steps

--- steps/base.go
+++ steps/base.go
@@ -1,8 +1,17 @@
 package steps
 
 import (
+	"fmt"
+
+	"github.com/facebookgo/stack"
+	"github.com/fatih/color"
 	"github.com/rai-project/uuid"
 	"golang.org/x/net/context"
+)
+
+var (
+	StackSize       = 4 << 10 // 4 KB
+	DisableStackAll = false
 )
 
 type base struct {
@@ -20,7 +29,15 @@ func (p base) Run(ctx context.Context, in <-chan interface{}, out chan interface
 		defer close(out)
 		defer func() {
 			if r := recover(); r != nil {
-				log.WithField("step", p.Info()).Error("recovered in %+v", r)
+				var err error
+				switch r := r.(type) {
+				case error:
+					err = r
+				default:
+					err = fmt.Errorf("%v", r)
+				}
+				stack := stack.Callers(3)
+				log.WithError(err).WithField("step", p.Info()).Errorf("[%s] %v\n", color.RedString("PANIC RECOVER"), stack)
 			}
 		}()
 		for {
