8517560a16e7be939033b19b85231ab6bcb7d15d
Cheng Li <cli99@illinois.edu>
2017-11-07 01:58:58 -0600

    add dataset download

---
+++ framework/cmd/server/download_dataset.go
@@ -0,0 +1,75 @@
+package server
+
+import (
+	"context"
+	"strings"
+
+	"github.com/k0kubun/pp"
+	"github.com/rai-project/dldataset"
+	"github.com/spf13/cobra"
+)
+
+var (
+	downloadDatasetCategory string
+	downloadDatasetName     string
+	downloadAll             bool
+)
+
+func datasetDownload(ctx context.Context, datasetCategory, datasetName string) error {
+	dataset, err := dldataset.Get(datasetCategory, datasetName)
+	if err != nil {
+		return err
+	}
+	defer dataset.Close()
+
+	err = dataset.Download(ctx)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func datasetDownloadAll(ctx context.Context) error {
+	datasets := dldataset.Datasets()
+	for _, dataset := range datasets {
+		splt := strings.Split(dataset, "/")
+		category, name := splt[0], splt[1]
+		pp.Println(splt)
+		err := datasetDownload(ctx, category, name)
+		if err != nil {
+			log.WithError(err).Errorf("failed to download %v", dataset)
+			return err
+		}
+	}
+
+	return nil
+}
+
+var downloadDatasetCmd = &cobra.Command{
+	Use:   "downloadDataset",
+	Short: "download datasets",
+	RunE: func(c *cobra.Command, args []string) error {
+		ctx := context.Background()
+
+		if downloadAll == true {
+			err := datasetDownloadAll(ctx)
+			if err != nil {
+				return err
+			}
+		}
+
+		err := datasetDownload(ctx, downloadDatasetCategory, downloadDatasetName)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	downloadDatasetCmd.PersistentFlags().StringVar(&downloadDatasetCategory, "dataset_category", "vision", "dataset category (e.g. \"vision\")")
+	downloadDatasetCmd.PersistentFlags().StringVar(&downloadDatasetName, "dataset_name", "ilsvrc2012_validation_224", "dataset name (e.g. \"ilsvrc2012_validation_folder\")")
+	downloadDatasetCmd.PersistentFlags().BoolVar(&downloadAll, "dowloadAll", false, "download all the available datasets")
+}
