e15d6d812c0bb3ba38a459212f0512d3ddf40559
Abdul Dakkak <abduld@wolfram.com>
2017-11-23 13:44:43 -0600

    add tensorflow evals

---
+++ framework/cmd/server/tensorflow.go
@@ -0,0 +1,30 @@
+// +build ignore
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	cmd "github.com/rai-project/dlframework/framework/cmd/server"
+	"github.com/rai-project/tensorflow"
+	_ "github.com/rai-project/tensorflow/predict"
+	"github.com/rai-project/tracer"
+
+	_ "github.com/rai-project/tracer/all"
+)
+
+func main() {
+
+	rootCmd, err := cmd.NewRootCommand(tensorflow.FrameworkManifest)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+
+	defer tracer.Close()
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
