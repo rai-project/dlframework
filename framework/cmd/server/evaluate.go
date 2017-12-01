8217a93a93235d268d389bbe37dbe7471a1a962f
Abdul Dakkak <abduld@wolfram.com>
2017-12-01 14:53:15 -0600

    udpate examples

--- framework/cmd/server/evaluate.go
+++ framework/cmd/server/evaluate.go
@@ -54,13 +54,13 @@ var (
 		"Inception_4.0",
 		"VGG19_1.0",
 	}
-	models = models_tf
+	models = models_c
 
 	frameworks = []string{
 		// "caffe",
-		// "mxnet",
+		"mxnet",
 		// "caffe2",
-		"tensorflow",
+		// "tensorflow",
 		// "tensorrt",
 	}
 	batchSizes = []int{
