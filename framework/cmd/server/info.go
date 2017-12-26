b91a92591b50238eb46d83aba7104fbe757d288f
Abdul Dakkak <abduld@wolfram.com>
2017-12-26 07:47:46 +0000

    add arc cmd

--- framework/cmd/server/info.go
+++ framework/cmd/server/info.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"os"
+
 	dllayer "github.com/rai-project/dllayer/cmd"
 	evaluations "github.com/rai-project/evaluation/cmd"
 	"github.com/spf13/cobra"
@@ -14,7 +16,27 @@ var infoCmd = &cobra.Command{
 	},
 }
 
+var infoMLArcCmd = &cobra.Command{
+	Use: "mlarc",
+	Aliases: []string{
+		"ml-arc",
+		"all",
+		"mlarc-web",
+	},
+	Short: "Get mlarc information from CarML",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		dllayer.FlopsInfoCmd.SetArgs(os.Args[2:])
+		evaluations.EvaluationCmd.SetArgs(append([]string{"all"}, os.Args[2:]...))
+
+		dllayer.FlopsInfoCmd.Execute()
+		evaluations.EvaluationCmd.Execute()
+
+		return nil
+	},
+}
+
 func init() {
 	infoCmd.AddCommand(dllayer.FlopsInfoCmd)
 	infoCmd.AddCommand(evaluations.EvaluationCmd)
+	infoCmd.AddCommand(infoMLArcCmd)
 }
