1937fd7e0ddbc461b99e0fa0ee5161ba286ac82c
Abdul Dakkak <dakkak@illinois.edu>
2017-12-28 16:06:52 +0000

    tegra

---
+++ framework/cmd/server/run_tegra.sh
@@ -0,0 +1,32 @@
+go run evaluate.go --tracer_address=minsky1-1.csl.illinois.edu:16686 --database_name=tx2_carml_step_trace --database_address=minsky1-1.csl.illinois.edu --publish_predictions=false --num_file_parts=8 --trace_level="STEP_TRACE";
+go run evaluate.go --tracer_address=minsky1-1.csl.illinois.edu:16686 --database_name=tx2_carml_step_trace --database_address=minsky1-1.csl.illinois.edu --publish_predictions=false --num_file_parts=16 --trace_level="STEP_TRACE";
+go run evaluate.go --tracer_address=minsky1-1.csl.illinois.edu:16686 --database_name=tx2_carml_step_trace --database_address=minsky1-1.csl.illinois.edu --publish_predictions=false --num_file_parts=32 --trace_level="STEP_TRACE";
+go run evaluate.go --tracer_address=minsky1-1.csl.illinois.edu:16686 --database_name=tx2_carml_step_trace --database_address=minsky1-1.csl.illinois.edu --publish_predictions=false --num_file_parts=64 --trace_level="STEP_TRACE";
+
+go run evaluate.go --tracer_address=minsky1-1.csl.illinois.edu:16686 --database_name=tx2_carml_full_trace --database_address=minsky1-1.csl.illinois.edu --publish_predictions=false --num_file_parts=8 --trace_level="FULL_TRACE";
+go run evaluate.go --tracer_address=minsky1-1.csl.illinois.edu:16686 --database_name=tx2_carml_full_trace --database_address=minsky1-1.csl.illinois.edu --publish_predictions=false --num_file_parts=8 --trace_level="FULL_TRACE";
+go run evaluate.go --tracer_address=minsky1-1.csl.illinois.edu:16686 --database_name=tx2_carml_full_trace --database_address=minsky1-1.csl.illinois.edu --publish_predictions=false --num_file_parts=8 --trace_level="FULL_TRACE";
+go run evaluate.go --tracer_address=minsky1-1.csl.illinois.edu:16686 --database_name=tx2_carml_full_trace --database_address=minsky1-1.csl.illinois.edu --publish_predictions=false --num_file_parts=8 --trace_level="FULL_TRACE";
+
+go run evaluate.go --tracer_address=minsky1-1.csl.illinois.edu:16686 --database_name=tx2_carml_step_trace --database_address=minsky1-1.csl.illinois.edu --publish_predictions=false --num_file_parts=8 --trace_level="STEP_TRACE";
+go run evaluate.go --tracer_address=minsky1-1.csl.illinois.edu:16686 --database_name=tx2_carml_step_trace --database_address=minsky1-1.csl.illinois.edu --publish_predictions=false --num_file_parts=16 --trace_level="STEP_TRACE";
+go run evaluate.go --tracer_address=minsky1-1.csl.illinois.edu:16686 --database_name=tx2_carml_step_trace --database_address=minsky1-1.csl.illinois.edu --publish_predictions=false --num_file_parts=32 --trace_level="STEP_TRACE";
+go run evaluate.go --tracer_address=minsky1-1.csl.illinois.edu:16686 --database_name=tx2_carml_step_trace --database_address=minsky1-1.csl.illinois.edu --publish_predictions=false --num_file_parts=64 --trace_level="STEP_TRACE";
+
+
+go run evaluate.go --tracer_address=minsky1-1.csl.illinois.edu:16686 --database_name=tx2_carml_full_trace --database_address=minsky1-1.csl.illinois.edu --publish_predictions=false --num_file_parts=8 --trace_level="FULL_TRACE";
+go run evaluate.go --tracer_address=minsky1-1.csl.illinois.edu:16686 --database_name=tx2_carml_full_trace --database_address=minsky1-1.csl.illinois.edu --publish_predictions=false --num_file_parts=8 --trace_level="FULL_TRACE";
+go run evaluate.go --tracer_address=minsky1-1.csl.illinois.edu:16686 --database_name=tx2_carml_full_trace --database_address=minsky1-1.csl.illinois.edu --publish_predictions=false --num_file_parts=8 --trace_level="FULL_TRACE";
+go run evaluate.go --tracer_address=minsky1-1.csl.illinois.edu:16686 --database_name=tx2_carml_full_trace --database_address=minsky1-1.csl.illinois.edu --publish_predictions=false --num_file_parts=8 --trace_level="FULL_TRACE";
+
+go run evaluate.go --tracer_address=minsky1-1.csl.illinois.edu:16686 --database_name=tx2_carml_step_trace --database_address=minsky1-1.csl.illinois.edu --publish_predictions=false --num_file_parts=8 --trace_level="STEP_TRACE";
+go run evaluate.go --tracer_address=minsky1-1.csl.illinois.edu:16686 --database_name=tx2_carml_step_trace --database_address=minsky1-1.csl.illinois.edu --publish_predictions=false --num_file_parts=16 --trace_level="STEP_TRACE";
+go run evaluate.go --tracer_address=minsky1-1.csl.illinois.edu:16686 --database_name=tx2_carml_step_trace --database_address=minsky1-1.csl.illinois.edu --publish_predictions=false --num_file_parts=32 --trace_level="STEP_TRACE";
+go run evaluate.go --tracer_address=minsky1-1.csl.illinois.edu:16686 --database_name=tx2_carml_step_trace --database_address=minsky1-1.csl.illinois.edu --publish_predictions=false --num_file_parts=64 --trace_level="STEP_TRACE";
+
+
+go run evaluate.go --tracer_address=minsky1-1.csl.illinois.edu:16686 --database_name=tx2_carml_step_trace --database_address=minsky1-1.csl.illinois.edu --publish_predictions=false --num_file_parts=8 --trace_level="STEP_TRACE";
+go run evaluate.go --tracer_address=minsky1-1.csl.illinois.edu:16686 --database_name=tx2_carml_step_trace --database_address=minsky1-1.csl.illinois.edu --publish_predictions=false --num_file_parts=16 --trace_level="STEP_TRACE";
+go run evaluate.go --tracer_address=minsky1-1.csl.illinois.edu:16686 --database_name=tx2_carml_step_trace --database_address=minsky1-1.csl.illinois.edu --publish_predictions=false --num_file_parts=32 --trace_level="STEP_TRACE";
+go run evaluate.go --tracer_address=minsky1-1.csl.illinois.edu:16686 --database_name=tx2_carml_step_trace --database_address=minsky1-1.csl.illinois.edu --publish_predictions=false --num_file_parts=64 --trace_level="STEP_TRACE";
+
