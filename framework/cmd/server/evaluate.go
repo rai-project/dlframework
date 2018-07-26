dbdd2ae44bda571a2ab35fe4344471c7b0932518
abhiutd <abhibits2012@gmail.com>
2018-07-26 18:19:36 -0500

    eval script failing with seg-fault

--- framework/cmd/server/evaluate.go
+++ framework/cmd/server/evaluate.go
@@ -30,28 +30,28 @@ var (
 
 	frameworks = []string{
 		"mxnet",
-		"caffe2",
-		"tensorflow",
-		"caffe",
-		"cntk",
-		"tensorrt",
+		//"caffe2",
+		//"tensorflow",
+		//"caffe",
+		//"cntk",
+		//"tensorrt",
 	}
 
 	models = []string{
 		"SqueezeNet_1.0",
-		"SqueezeNet_1.1",
-		"BVLC-AlexNet_1.0",
-		"BVLC-Reference-CaffeNet_1.0",
-		"BVLC-GoogLeNet_1.0",
-		"ResNet101_1.0",
-		"ResNet101_2.0",
-		"WRN50_2.0",
-		"BVLC-Reference-RCNN-ILSVRC13_1.0",
-		"Inception_3.0",
-		"Inception_4.0",
-		"ResNeXt50-32x4d_1.0",
-		"VGG16_1.0",
-		"VGG19_1.0",
+		//"SqueezeNet_1.1",
+		//"BVLC-AlexNet_1.0",
+		//"BVLC-Reference-CaffeNet_1.0",
+		//"BVLC-GoogLeNet_1.0",
+		//"ResNet101_1.0",
+		//"ResNet101_2.0",
+		//"WRN50_2.0",
+		//"BVLC-Reference-RCNN-ILSVRC13_1.0",
+		//"Inception_3.0",
+		//"Inception_4.0",
+		//"ResNeXt50-32x4d_1.0",
+		//"VGG16_1.0",
+		//"VGG19_1.0",
 	}
 
 	batchSizes = []int{
@@ -134,7 +134,7 @@ func main() {
 							continue
 						}
 						fmt.Println("Running " + shellCmd)
-						cmd := exec.Command(filepath.Join(sourcePath, framework), args...)
+						cmd := exec.Command(filepath.Join(sourcePath, "main"), args...)
 						cmd.Stdout = os.Stdout
 						cmd.Stderr = os.Stderr
 
