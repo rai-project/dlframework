d6962776158916aefd1f1a893d0d2aa4a165711d
Abdul Dakkak <dakkak@illinois.edu>
2018-09-16 00:56:07 +0000

    updates

--- framework/cmd/server/predict_dataset.go
+++ framework/cmd/server/predict_dataset.go
@@ -59,25 +59,14 @@ var (
 func newProgress(prefix string, count int) *pb.ProgressBar {
 	// get the new original progress bar.
 	//bar := pb.New(count).Prefix(prefix)
-        // TODO: set prefix of bar
-        bar := pb.New(count)
-        //bar.Set("prefix", prefix)
+	// TODO: set prefix of bar
+	bar := pb.New(count)
+	//bar.Set("prefix", prefix)
 
 	// Refresh rate for progress bar is set to 100 milliseconds.
 	bar.SetRefreshRate(time.Millisecond * 100)
 
-	// Use different unicodes for Linux, OS X and Windows.
-	switch runtime.GOOS {
-	case "linux":
-		// Need to add '\x00' as delimiter for unicode characters.
-		bar.Format("┃\x00▓\x00█\x00░\x00┃")
-	case "darwin":
-		// Need to add '\x00' as delimiter for unicode characters.
-		bar.Format(" \x00▓\x00 \x00░\x00 ")
-	default:
-		// Default to non unicode characters.
-		bar.Format("[=> ]")
-	}
+	bar.SetTemplateString(string(pb.Full))
 	bar.Start()
 	return bar
 }
@@ -223,8 +212,8 @@ var datasetCmd = &cobra.Command{
 
 		evaluationEntry := evaluation.Evaluation{
 			ID:                  bson.NewObjectId(),
-			UserID:							 userID,
-			RunID:							 runID,
+			UserID:              userID,
+			RunID:               runID,
 			CreatedAt:           time.Now(),
 			Framework:           *model.GetFramework(),
 			Model:               *model,
@@ -332,9 +321,9 @@ var datasetCmd = &cobra.Command{
 			for o := range output {
 				if err, ok := o.(error); ok && failOnFirstError {
 					//inferenceProgress.FinishPrint("inference halted")
-				        inferenceProgress.Finish()
+					inferenceProgress.Finish()
 
-                                        log.WithError(err).Error("encountered an error while performing inference")
+					log.WithError(err).Error("encountered an error while performing inference")
 					os.Exit(-1)
 				}
 				outputs <- o
@@ -347,7 +336,7 @@ var datasetCmd = &cobra.Command{
 		}()
 
 		//inferenceProgress.FinishPrint("inference complete")
-                inferenceProgress.Finish()
+		inferenceProgress.Finish()
 
 		close(outputs)
 
@@ -405,7 +394,7 @@ var datasetCmd = &cobra.Command{
 		}
 
 		//databaseInsertProgress.FinishPrint("inserting prediction complete")
-                databaseInsertProgress.Finish()
+		databaseInsertProgress.Finish()
 
 		modelAccuracy := evaluation.ModelAccuracy{
 			ID:        bson.NewObjectId(),
