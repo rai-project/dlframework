500b4fa014b7b1cc076708005a3108957f48ebd7
abhiutd <abhibits2012@gmail.com>
2018-10-12 15:54:12 -0500

    add dummy database

--- http/signup.go
+++ http/signup.go
@@ -5,7 +5,30 @@ import (
   "github.com/go-openapi/runtime/middleware"
 	"github.com/pkg/errors"
   "github.com/rai-project/dlframework/httpapi/restapi/operations/signup"
+	"github.com/rai-project/passlib"
 )
+
+// dummy for MongoDB User Database
+type userRecord struct {
+	firstname		string
+	lastname		string
+	username		string
+	password		string
+	affiliation	string
+}
+var userTable = map[string]*userRecord{}
+
+func GenerateHash(password string) string {
+
+	hash, err := passlib.Hash(password)
+	if err != nil {
+		// couldn't hash password for some reason
+		return "xxx"
+	}
+
+	return hash
+
+}
 
 func SignupHandler(params signup.SignupParams) middleware.Responder {
 
@@ -15,11 +38,16 @@ func SignupHandler(params signup.SignupParams) middleware.Responder {
 	password := params.Body.Password
 	affiliation := params.Body.Affiliation
 
-	// check if first name/last name/affiliation matches in the DB
-	// check if username already exists
 	if (len(firstName) == 0 || len(lastName) == 0 || len(username) == 0 || len(password) == 0 || len(affiliation) == 0) {
     return NewError("Signup", errors.New("Incomplete Information"))
   }
+
+	encryption := GenerateHash(password)
+	if encryption == "xxx" {
+		return NewError("Signup", errors.New("Could not generate password"))
+	}
+
+	userTable[username] = &userRecord{firstname: firstName, lastname: lastName, username: username, password: encryption, affiliation: affiliation}
 
 	return signup.NewSignupOK()
 
