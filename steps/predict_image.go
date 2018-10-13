5dafa0ea15669d6036e09becaa4c1d5ec78367a2
Cheng Li <cli99@illinois.edu>
2018-10-13 14:32:39 -0500

    s

--- steps/predict_image.go
+++ steps/predict_image.go
@@ -59,7 +59,7 @@ func (p predictImage) do(ctx context.Context, in0 interface{}, pipelineOpts *pip
 		return err
 	}
 
-	span, ctx := tracer.StartSpanFromContext(ctx, tracer.APPLICATION_TRACE, "Predict", opentracing.Tags{
+	span, ctx := tracer.StartSpanFromContext(ctx, tracer.APPLICATION_TRACE, p.Info() opentracing.Tags{
 		"model_name":        model.GetName(),
 		"model_version":     model.GetVersion(),
 		"framework_name":    framework.GetName(),
@@ -78,7 +78,7 @@ func (p predictImage) do(ctx context.Context, in0 interface{}, pipelineOpts *pip
 		cu, err = cupti.New(cupti.Context(ctx))
 	}
 
-	handle, err := p.predictor.Predict(ctx, data, options.WithOptions(opts))
+	features, err := p.predictor.Predict(ctx, data, options.WithOptions(opts))
 	if err != nil {
 		if cu != nil {
 			cu.Wait()
@@ -92,12 +92,10 @@ func (p predictImage) do(ctx context.Context, in0 interface{}, pipelineOpts *pip
 		cu.Close()
 	}
 
-	panic("need to implement")
-	_ = handle
 	lst := make([]interface{}, len(data))
-	// for ii := 0; ii < len(in); ii++ {
-	// 	lst[ii] = features[ii]
-	// }
+	for ii := 0; ii < len(in); ii++ {
+		lst[ii] = features[ii]
+	}
 
 	return lst
 }
