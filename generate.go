4632330b6458b75408a5b99505c171452fffeb1c
Cheng Li <cli99@illinois.edu>
2018-11-07 23:07:46 -0600

    do not use easyjson

--- generate.go
+++
@@ -1,6 +0,0 @@
-//go:generate go get -v github.com/mailru/easyjson/...
-//go:generate easyjson -snake_case -all -pkg framework/agent
-//go:generate easyjson -snake_case -all -disallow_unknown_fields -pkg httpapi/models
-//go:generate easyjson -snake_case -disallow_unknown_fields -pkg .
-
-package dlframework
